7efaee684adefda7fd935c2d8d8fc388d1e242e8
Chris Howe <chris@howeville.com>
2012-03-20 04:32:29 +0000

    fixed basic test cases

--- vows/gestalt_vows.js
+++ vows/gestalt_vows.js
@@ -1,17 +1,20 @@
 
 var vows = require('vows'),
     assert = require('assert');
+var EventEmitter = require('events').EventEmitter;
 
 var config = require('../lib/gestalt');
 var Configuration = config.Configuration;
 var ConfigContainer = config.ConfigContainer;
 
-var x = vows.describe('gestalt').addBatch('Configuration Object', {
-    'A Configuration': {
-            topic: function() { return new Configuration("A Configuration Test");},
-            'should have get and set methods': function(config) {
-                assert.isFunction( config.get );
-                assert.isFunction( config.set );
+vows.describe('gestalt').addBatch(
+    {'Configuration Object': {
+            topic: new Configuration("A Configuration Test"),
+            'should have get and set methods': function(target) {
+                assert.isObject(target);
+                assert.equal(target._source_, 'A Configuration Test');
+                assert.isFunction( target.get );
+                assert.isFunction( target.set );
             },
             'should return undefined for an undefined value': function( config ) {
                 assert.isUndefined( config.get('non existant value') );
@@ -32,13 +35,69 @@ var x = vows.describe('gestalt').addBatch('Configuration Object', {
                 
                 assert.equal( config.get(name), val1 );
                 assert.equal( config.get(path), val2 );
-
+            
                 config.set(name, val2);
                 config.set(path, val1 );
                 
                 assert.equal( config.get(name), val2 );
                 assert.equal( config.get(path), val1 );
+        },
+    'Configuration Object Emitter': {
+        "first change": {
+            topic: function() {
+                var promise = new EventEmitter;
+                var config = new Configuration("Configuration Emitter");
+                config.on('change', function(name, value, source ) {
+                    promise.emit('success', {name: name, value:value, source:source })
+                });
+                config.set("name_n", "value_v", "source_s");
+                return promise;
+            },
+            "emits a 'change' when changed": function( obj ) {
+                assert.equal(obj.name, "name_n");
+                assert.equal(obj.value, "value_v");
+                assert.equal(obj.source, "source_s");
+                }
             }
-    }
-           
+        },
+        "second change": {
+            topic: function() {
+                var promise = new EventEmitter;
+                var config = new Configuration("Configuration Emitter");
+                var done = false;
+                config.set("name_n", "value_v", "source_s");
+                config.on('change', function(name, value, source ) {
+                    promise.emit('success', {name: name, value:value, source:source })
+                    done = true;
+                });
+                config.on('done', function() {
+                    if(!done);
+                    promise.emit('success', null);
+                });
+                config.set("name_n", "value_v", "source_s");
+                config.emit('done');
+                return promise;
+            },
+            "doesnt emit a 'change' when changed to the same value": function( obj ) {
+                assert.isNull(obj);
+            }
+
+        },
+        "path change": {
+            topic: function() {
+                var promise = new EventEmitter;
+                var config = new Configuration("Configuration Emitter");
+                config.on('change', function(name, value, source ) {
+                    promise.emit('success', {name: name, value:value, source:source })
+                });
+                config.set("path:to:somewhere", "a_value", "source_s");
+                return promise;
+            },
+            "emits a 'change' when changed": function( obj ) {
+                assert.equal(obj.name, "path:to:somewhere");
+                assert.equal(obj.value, "a_value");
+                assert.equal(obj.source, "source_s");
+            }
+        }
+    }           
 }).run();
