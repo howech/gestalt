bb6c16250ae6f99e2241a7926bc1267d6da25887
Chris Howe <chris@howeville.com>
2012-03-22 14:58:36 +0000

    working on container functionality

--- vows/gestalt_vows.js
+++ vows/gestalt_vows.js
@@ -2,6 +2,7 @@
 var vows = require('vows'),
     assert = require('assert');
 var EventEmitter = require('events').EventEmitter;
+var _ = require('underscore');
 
 var config = require('../lib/gestalt');
 var Configuration = config.Configuration;
@@ -46,17 +47,11 @@ exports.configuration.addBatch( {
                     var path = "a:long:path:name";
                     var value = "aasdfWdRC";
                     var subConfig;
-                    //assert.equal( config.get(path), value );
-                    //assert.equal( config.get( ["a","long","path","name"] ) , value );
                     config.set(path, value);                
-                    assert.instanceOf( config.get("a"), Configuration );
-                    assert.isFunction( config.get("a")._listeners_.long );
-                    assert.instanceOf( config.get("a:long"), Configuration );
-                    assert.isFunction( config.get("a:long")._listeners_.path );
+                    assert.equal( config.get(path), value );
+                    assert.equal( config.get( ["a","long","path","name"] ) , value );
                     subConfig = config.get("a:long:path");
-                    assert.instanceOf( subConfig, Configuration );
-                    assert.isFunction( subConfig.get);
-                    //assert.equal( subConfig.get("name"), value );
+                    assert.equal( subConfig.get("name"), value );
                 },
             'should allow removal of settings':
                 function(config) {
@@ -84,8 +79,30 @@ exports.configuration.addBatch( {
                     var objConfig = config.get("object");
                     var obj = objConfig.toObject();
                     assert.deepEqual( obj, objVal );
-                },        
+                }
         },
+    'configuration keys and each': {
+        topic: function() {
+            var config = new Configuration();
+            config.set("a", { b: '1', c: '2'} );
+            config.set("e", [ "f", "g", {h: 'i'} ] );
+            return config;            
+        },
+        'has keys': function(config) { 
+            assert.deepEqual( config.keys().sort(), ['a','a:b','a:c', 'e', 'e:0', 'e:1', 'e:2', 'e:2:h' ] ) ;
+        },
+        'each works': function(config) {
+            var keys = config.keys();
+            var each_keys = [];
+            
+            config.each( function(element, key, cfg) {
+                each_keys.push(key);
+                assert.equal( element, cfg.get(key) );
+            });
+            
+            assert.deepEqual( keys.sort(), each_keys.sort() );
+        }
+    },
     'Configuration events': {
         "first change": {
             topic: function() {
@@ -147,6 +164,11 @@ exports.configuration.addBatch( {
                 lop2.set("a", "qqq"); // not a change
                 lop1.set("x", "zzz"); // not a change
                                 
+                config.set("q", lop2); // change 6
+                config.set("r", lop2); // change 7
+                config.set("s", lop1); // change 8
+                
+                config.set("q:a", "qrqr" ); // changes 9 and 10
                 return changes;
             },
             "change for path:to:somewhere:a": function( changes ) { 
@@ -176,9 +198,175 @@ exports.configuration.addBatch( {
                 assert.isUndefined( changes[5].value );
                 assert.equal( changes[5].name, "path:to:nowhere" );
             },
-            "thats all folks": function(changes) { assert.lengthOf( changes, 6 ); }
+            "add lop2 back to tree twice": function(changes) {
+                assert.equal( changes[6].name, "q" );
+                assert.equal( changes[7].name, "r" );
+                assert.isUndefined( changes[6].old_value );
+                assert.isUndefined( changes[7].old_value );
+                assert.equal( changes[6].value, changes[7].value );
+            },
+            "add lop1 back to tree": function(changes) {
+                assert.equal( changes[8].name, "s" );
+                assert.isUndefined( changes[8].old_value );
+            },
+            "change in lop2 now generates two changes in treee": function(changes) {
+                assert.equal( changes[9].value, changes[10].value );
+                assert.deepEqual( [ changes[9].name, changes[10].name].sort(), ["q:a", "r:a" ] );
+            },
+            "thats all folks": function(changes) { assert.lengthOf( changes, 11 ); }
+        },
+        "_touch_ structured configs": {
+            topic: function() {
+                var changes = [];
+                var config = new Configuration("_touch_");
+                config.set("path:to:somewhere:a", "a_value");  
+                config.set("path:to:nowhere:x", "a_value");    
+                config.set("path:to:nowhere:y", "a_value");    
+                config.set("path:to:somewhere:a", "b_value");  
+                config.on('change', function( change ) { changes.push(change); });
+                config._touch_();
+                return { config: config, changes: changes };
+            },
+            'there is a change for every key':  function(topic) {
+                var change_keys = _.map( topic.changes, function( change ) { return change.name; } );
+                assert.lengthOf( topic.changes, topic.config.keys().length );
+                assert.deepEqual( change_keys.sort(), topic.config.keys().sort() );
+            },
+            'every change value agrees with config': function(topic) {
+                _.each( topic.changes, function(change) { assert.equal(change.value, topic.config.get(change.name) );} );
+            }
         }
     }
 });
+
+exports.configuration.addBatch( {
+    'config container object': {
+            topic: new ConfigContainer("config container basics"),
+            'should be an object':          function(config) { assert.instanceOf(config,ConfigContainer); },
+            'should be a Container':        function(config) { assert.instanceOf(config,Configuration); },
+            'should be an EventEmitter':    function(config) { assert.instanceOf(config,EventEmitter); },
+            'should have a get method':     function(config) { assert.isFunction( config.get ); },
+            'should have a set method':     function(config) { assert.isFunction( config.set ); },
+            'should have a remove method':  function(config) { assert.isFunction( config.remove ); },
+            'should return undefined for a new value': 
+                function(config) { assert.isUndefined( config.get('non existant value') ); },
+            'shoule return undefined for a new path':
+                function(config) { assert.isUndefined( config.get('non:existant:path') ); },
+            'should populate new values when set': 
+                function(config) {
+                    var name = "new name";
+                    var value = "my value";
+                    var value2 = "second";
+                    config.set(name, value);                
+                    assert.equal( config.get(name), value );
+                    config.set(name, value2);                
+                    assert.equal( config.get(name), value2 );
+                },
+            'should break apart colon separated paths':
+                function(config) {
+                    var path = config.getPath("a:b:c:d");
+                    assert.isArray( path );
+                    assert.equal(path.length, 4);
+                    assert.equal(path[0], "a");
+                    assert.equal(path[1], "b");
+                    assert.equal(path[2], "c");
+                    assert.equal(path[3], "d");
+                },
+            'should populate new paths when set': 
+                function(config) {
+                    var path = "a:long:path:name";
+                    var value = "aasdfWdRC";
+                    config.set(path, value);
+                    assert.equal( config.get(path), value );
+                },
+            'should allow removal of settings':
+                function(config) {
+                    config.set("doomed","xxx");
+                    config.set("youre:gonna:die","zzz");
+                    config.remove("doomed");
+                    config.remove("youre:gonna");
+                    assert.isUndefined( config.get("doomed") );
+                    assert.isUndefined( config.get("youre:gonna:die") );
+                },
+            'should destructure values when set':
+                function(config) {
+                    config.set("structured", { a: "a", b: "b", c: [1,2,3], d: {e: "f"} });
+                    assert.equal( config.get("structured:a") , "a" );
+                    assert.equal( config.get("structured:b") , "b" );                    
+                    assert.equal( config.get("structured:c:0") , 1 );
+                    assert.equal( config.get("structured:c:1") , 2 );
+                    assert.equal( config.get("structured:c:2") , 3 );
+                    assert.equal( config.get("structured:d:e") , "f" );
+                },
+            'can be converted to a plain object':
+                function(config) {
+                    var objVal = { a: "a", b: "b", c: [1,2,3], d: {e: "f"} };
+                    config.set("object", { a: "a", b: "b", c: [1,2,3], d: {e: "f"} });
+                    var objConfig = config.get("object");
+                    var obj = objConfig.toObject();
+                    assert.deepEqual( obj, objVal );
+                }
+        },
+    'configuration keys and each': {
+        topic: function() {
+            var config = new Configuration();
+            config.set("a", { b: '1', c: '2'} );
+            config.set("e", [ "f", "g", {h: 'i'} ] );
+            return config;            
+        },
+        'has keys': function(config) { 
+            assert.deepEqual( config.keys().sort(), ['a','a:b','a:c', 'e', 'e:0', 'e:1', 'e:2', 'e:2:h' ] ) ;
+        },
+        'each works': function(config) {
+            var keys = config.keys();
+            var each_keys = [];
+            
+            config.each( function(element, key, cfg) {
+                each_keys.push(key);
+                assert.equal( element, cfg.get(key) );
+            });
+            
+            assert.deepEqual( keys.sort(), each_keys.sort() );
+        }
+    },
+    'overrides and defaults basics': {
+        "first change": {
+            topic: function() {
+                var override = new Configuration('override');
+                var config = new Configuration('config');
+                var defaults = new Configuration('default');
+                var container = new ConfigContainer( 'container', {}, config );
+                container.addOverride( override );
+                container.addDefault( defaults );
+                
+                override.set( "a" , "override" );
+                override.set( "b", "override" );
+                override.set("f", "override" );
+                config.set( "a", "masked");
+                config.set( "c", "normal");
+                defaults.set("b", "masked");
+                defaults.set("c", "masked");
+                defaults.set("d","defauilt");
+                container.set("e", "container");
+                container.set("f", "masked");
+    
+                return container;
+            },
+            'internal structure of container': function(container) {
+                assert.lengthOf( container._contents_, 3 );
+            },
+            'overrides take precedence, defaults fill in gaps': function(container) {
+                assert.equal( container.get("a"), "override" );
+                assert.equal( container.get("b"), "override" );
+                //assert.equal( container.get("c"), "normal" );
+                assert.equal( container.get("d"), "default" );
+                assert.equal( container.get("e"), "container" );
+                assert.equal( container.get("f"), "override" );
+            },
+        }
+    }
+});
+
+
 
 exports.configuration.run();
