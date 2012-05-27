e729ef30f9204f325df43e8dcd596ae1e5b7812d
Chris Howe <chris@howeville.com>
2012-05-27 14:52:53 -0500

    beefed up befhavior for 'undefined' as a key, and added a 'remap' config node

---
+++ examples/remap-example.js
@@ -0,0 +1,17 @@
+var ConfigEnv = require('../lib/env').ConfigEnv;
+var RemapConfig = require('../lib/remap').RemapConfig; 
+
+function mapper(old) {
+    if( old.match(/^f/ )) {
+	return "new:" + old;	
+    } else {
+	return undefined;
+    }
+}
+
+var c = new ConfigEnv();
+var r = new RemapConfig( mapper, c );
+
+console.log( r.get('new:foo') );
+console.log( r.keys() );
+r.report();
