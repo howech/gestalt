826be6f4410508c6b2b420e1bd16beeffd410bc0
Chris Howe <chris@howeville.com>
2012-05-30 22:05:11 -0500

    neatened up cluster.js a little bit.

--- examples/cluster.js
+++ examples/cluster.js
@@ -42,6 +42,11 @@ config.addDefault( zookeeper );
 config.addOverride( envMap );
 config.addOverride( argsMap );
 
+config.patternListen(/^cluster:name$/, function(change) {
+    if(change.value) {
+	join_cluster();
+    }
+});    
 
 config.patternListen(/^zk:children:leader:data$/, function(change) {
     if( change.value ) {
@@ -50,6 +55,47 @@ config.patternListen(/^zk:children:leader:data$/, function(change) {
 	console.log("There is no leader.");
     }
 });
+
+function election_path() {
+    return "/" + config.get("cluster:name" ) + config.get("zookeeper:election" );
+}
+
+function config_path() {
+    return "/" + config.get("cluster:name" ) + config.get("zookeeper:config" );
+}
+
+function names_path() {
+    return "/" + config.get("cluster:name" ) + config.get("zookeeper:names" );
+}
+
+
+// Our name in the cluster is stored at cluster:me
+config.patternListen(/^cluster:me$/, function(change) {
+    if(change.value) {
+	var name = change.value;
+	console.log( "I am", name );
+	var zk_config = config.get("zk");
+	// having a name set should indicate that
+	// we have a valid zk_config
+	if( !zk_config ) {
+	    throw new Error("Expected a zk_config");
+	}
+
+	zk_config.zookeeper( function(zk) {
+	    zk_cast_ballot(zk,election_path(), name, function() {
+		// become leader
+		console.log("I am leader");		
+		zk.a_create( config_path() + "/leader", config.get("cluster:me"), 1, function(rc,err,path) {
+		    if(rc) {
+			throw new Error("Error becoming leader" + err);
+		    }
+		});
+	    });
+	});	
+    }
+});
+
+
 
 //console.log( require.resolve( config.get('config:file')));
 var file = new gestalt.ConfigFile( { source: require.resolve( config.get( 'config:file' ) ),
@@ -64,53 +110,66 @@ file.on('invalid', function(err, source) {
 });
 config.addDefault( file );
 
-function on_load_config_file() {
-    var zk_connect = config.get("zookeeper:options:connect");
-    var cluster_name = config.get("cluster:name");
-    
-    var cluster_url = zk_connect+"/"+cluster_name;
-    var election_path = "/" + cluster_name + config.get("zookeeper:election");
-    var names_path = "/" + cluster_name + config.get("zookeeper:names");
-    var config_url = cluster_url + config.get("zookeeper:config");
-    var config_path = "/" + cluster_name + config.get("zookeeper:config");
 
-    //console.log(config_url);
-    var zk_config = new gestalt.ZookeeperConfig({
+function on_load_config_file() {
+    join_cluster();
+}
+
+
+function join_cluster() {
+    // This method currently gets triggered
+    // a few times prematurely. We must
+    // ensure that there is in fact a cluster name
+    // and some names to choose from before we
+    // really start to join.
+    //
+    // Seems to come from a but in the ConfigFile
+    // loaded evetn...
+    var cluster_name = config.get("cluster:name");
+    if( ! cluster_name ) {
+	//console.log("missing cluster name.");
+	// Maybe we will get one later...
+	return;
+    }
+    var names = config.get("names");
+    if( !names ) {
+	//console.log("no names to choose from");
+	return;
+    }
+
+    var zk_config = config.get("zk");
+    if( zk_config ) {
+	// Clean up old zk_config object
+	zk_config.zookeeper( function(zk) { zk.close(); });
+    }
+    var zk_connect = config.get("zookeeper:options:connect");
+
+    console.log("\nJoining cluster: %s", cluster_name);
+
+    var config_url = zk_connect + config_path();
+
+    // set up zk_config object
+    zk_config = new gestalt.ZookeeperConfig({
 	source: config_url, 
 	create_paths: true,
 	format: 'raw'
-    });
+    }).on('invalid', function(err){ throw new Error(err) });
     
     zookeeper.set( "zk", zk_config );
-    zk_config.on('invalid', function(err){ console.log(err)} );
-    
+ 
+    // Negotiate a name
+
     zk_config.zookeeper( function(zk) { 
-	zk_name_me(zk, names_path, config.get("names").toObject(),function(name) {
-	    console.log("I am",name);
+	zk_name_me(zk, names_path(), names.toObject(), function(name) {
 	    config.set("cluster:me",name);
 	}); 
     });
-    
-    // When we get a name, cast a ballot to become the leader
-    config.patternListen( /^cluster:me$/, function(change) {
-	if(change.value) {
-	    var name = change.value;
-	    zk_config.zookeeper( function(zk) {
-		zk_cast_ballot(zk,election_path,name, function() {
-		    // become leader
-		    console.log("I am leader");
-		    zk.a_create( config_path + "/leader", config.get("cluster:me"), 1, function() {} );
-		});
-	    });
-	}
-    });
-    
-    // Watch for a change in the leader
-//    config.patternListen(/^children:leader:data$/, function(change) {
-//	console.log( change.value, "is now leader.");
-//    });
 }
 
+
+// Zookeeper Stuff
+//
+// Negotiate a unique name
 function zk_choose_name(zk,dir,names,cb) {
     zk.a_get_children(dir,false,function(rc,err,children) {
 	var n1 = _.without(names,children);
@@ -120,7 +179,7 @@ function zk_choose_name(zk,dir,names,cb) {
 	    var name = n1[ Math.floor( Math.random() * n1.length ) ];
 	    zk.a_create( dir+"/"+name, "", 1, function(rc,err,stat,path) {
 		if(rc) {
-		    zk_choose_name(zk,dir,names);
+		    zk_choose_name(zk,dir,names,cb);
 		} else {
 		    cb( name );
 		}
@@ -130,16 +189,15 @@ function zk_choose_name(zk,dir,names,cb) {
 }    
 
 function zk_name_me(zk,dir,names,cb) {
-    zk.a_create( dir, '', 0, function(rc,err,path) {
-	if(rc && rc != ZooKeeper.ZNODEEXISTS ) {
-	    console.log({rc:rc, err:err, path:path});
-	    throw new Error("Something wrong")
-	} else {
-	    zk_choose_name(zk,dir,names,cb);
+    zk_ensure_path(zk, dir, function(err) {
+	if(err) {
+	    throw err;
 	}
+	zk_choose_name(zk,dir,names,cb);
     });
 } 
 
+// Negotiate to become the leader
 function zk_watch_leader(zk,dir,me,path,cb) {
     var lw = function(type, state, p){
 	if( type == ZooKeeper.ZOO_CHANGED_EVENT ) {
@@ -154,13 +212,16 @@ function zk_watch_leader(zk,dir,me,path,cb) {
 	    // zk will keep our callback around - no need to re-establish.
 	} else {
 	    console.log( "Unkown type: " + type );
-	    //zk.aw_get( path, lw, function() {} );
+	    throw new Error("Unknown change type");
 	} 
     };
     zk.aw_get( path, lw, function() {} ); 
 }
 
 function zk_election(zk, dir, me, cb) {
+    if(!cb) {
+	throw new Error("zk_election must have a callback");
+    }
     zk.a_get_children(dir, false, function(rc,err,children) {
 	children.sort();
 	var last = null;
@@ -179,40 +240,58 @@ function zk_election(zk, dir, me, cb) {
 }
 
 function zk_cast_ballot(zk,dir,name,cb) {
-    //create the election dir
-    zk.a_create( dir, "", 0, function(rc,error,path) {
-	if(rc && rc != ZooKeeper.ZNODEEXISTS ) {
-	    console.log("unable to create election directory");
-	    // TODO: handle error
-	} else {
-	    zk.a_create(dir + "/n",name, ZooKeeper.ZOO_SEQUENCE + ZooKeeper.ZOO_EPHEMERAL,function (rc, error, path)  {
-		if (rc != 0) {
-		    console.log ("zk node create result: %d, error: '%s', path=%s", rc, error, path);
- 		} else {
- 		    var me = path.match(/[^\/]+$/)[0];
- 		    zk_election(zk,dir,me,cb);
- 		}
-	    });
+    zk_ensure_path(zk, dir, function(err) {
+	if(err) {
+	    throw err;
 	}
+	zk.a_create(dir + "/n",name, ZooKeeper.ZOO_SEQUENCE + ZooKeeper.ZOO_EPHEMERAL,function (rc, error, path)  {
+	    if (rc != 0) {
+		throw new Error("Unable to create ballot node");
+ 	    } else {
+ 		var me = path.match(/[^\/]+$/)[0];
+ 		zk_election(zk,dir,me,cb);
+ 	    }
+	});
     });
 }
 
-
+function zk_ensure_path(zk, path, cb ) {
+    var segments = path.split('/');
+    segments.shift();
+    p = "";
+    var next = function(rc,err,pth) {
+	if( rc && rc != ZooKeeper.ZNODEEXISTS ) {
+	    cb( new Error("Unable to create node: " + path ) );
+	} else {
+	    if( segments.length > 0 ) {
+		p = p + "/" + segments.shift();
+		zk.a_create( p, '', 0, next );
+	    } else {
+		cb();
+	    }
+	}
+    };
+    next();
+}
+// CTRL-C twice quickly to exit
+//        once to dump a status report
 
 var setup_exit = function() {
     f = function() { process.exit(); }
     process.removeAllListeners('SIGINT');
     process.on('SIGINT', f );
-    setTimeout( function() { process.removeListener('SIGINT', f); setup_report();}, 300);
+    setTimeout( function() { 
+	process.removeListener('SIGINT', f); 
+	setup_report();
+        config.report();
+    }, 300);
 };
 
 var setup_report = function() {
     process.on('SIGINT', function () {
-        config.report("  ");
         setup_exit();
     });
 };
 
 setup_report();
 
-//config.report();
