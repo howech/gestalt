48cbda0dc8b14b0faad627deb8fec8d14ef0f698
Chris Howe <chris@howeville.com>
2012-06-10 11:52:11 -0500

    added test cases for auto format and custom parser

--- test/files_vows.js
+++ test/files_vows.js
@@ -33,6 +33,27 @@ vows.describe( "Gestalt Configuration File Object").addBatch( {
                 assert.deepEqual( config.toObject(), { "test1": "a", "test2": "b", "test3": {"c": "d", "e": ["f","g","h"] } , "test4": "i" } );
             }        
         },
+        "json file, auto": {
+            topic: function() {
+                var promise = new EventEmitter();
+                var config_json = require.resolve('./files/config.json');
+                var config = new ConfigFile( {source: config_json } );
+		config.on('state', function(state_change) {
+		    if( state_change.state == 'invalid' ) {
+			promise.emit('failure', config);
+		    } else if ( state_change.state == 'ready' ) {
+			promise.emit('success', config);
+		    }
+		});
+                return promise;
+            },
+            'loads properly': function(config) {
+                assert.instanceOf( config, Configuration);
+                assert.instanceOf( config, ConfigFile );
+                assert.deepEqual( config.toObject(), { "test1": "a", "test2": "b", "test3": {"c": "d", "e": ["f","g","h"] } , "test4": "i" } );
+            }        
+        },
+
         "yaml file": {
             topic: function() {
                 var promise = new EventEmitter();
@@ -55,6 +76,29 @@ vows.describe( "Gestalt Configuration File Object").addBatch( {
                 assert.deepEqual( config.toObject(), contents );
             }        
         },
+        "yaml file, auto": {
+            topic: function() {
+                var promise = new EventEmitter();
+                var config_yaml = require.resolve('./files/config.yaml');
+                var config = new ConfigFile( {source: config_yaml } );
+		config.on('state', function(state_change) {
+		    if( state_change.state == 'invalid' ) {
+			promise.emit('failure', config);
+		    } else if ( state_change.state == 'ready' ) {
+			promise.emit('success', config);
+		    }
+		});
+                return promise;
+            },
+            'loads properly': function(config) {
+                assert.instanceOf( config, Configuration);
+                assert.instanceOf( config, ConfigFile );
+                var contents = { test: [ {one:1, two:2, three:3}, {four:2, five:5,six:6}, {a: 'b', c: 'asdfasd' } ],
+                                  aaa: { bbb: { ccc: 'a' } } };                                                  
+                assert.deepEqual( config.toObject(), contents );
+            }        
+        },
+
         "ini file": {
             topic: function() {
                 var promise = new EventEmitter();
@@ -77,6 +121,55 @@ vows.describe( "Gestalt Configuration File Object").addBatch( {
                 var contents = { top: 'level', one: {stuff: 'more stuff'}, two: {x: 'y'} };
                 assert.deepEqual( config.toObject(), contents );
             }        
-        } //,
+        },
+        "ini file, auto": {
+            topic: function() {
+                var promise = new EventEmitter();
+                var config_ini = require.resolve('./files/config.ini');
+                var config = new ConfigFile( { source: config_ini } );
+		config.on('state', function(state_change) {
+		    if( state_change.state == 'invalid' ) {
+			promise.emit('failure', config);
+		    } else if ( state_change.state == 'ready' ) {
+			promise.emit('success', config);
+		    }
+		});
+
+                //config.emit('loaded');
+                return promise;
+            },
+            'loads properly': function(config) {
+                assert.instanceOf( config, Configuration);
+                assert.instanceOf( config, ConfigFile );
+                var contents = { top: 'level', one: {stuff: 'more stuff'}, two: {x: 'y'} };
+                assert.deepEqual( config.toObject(), contents );
+            }        
+        },
+        "json file, custom parser": {
+            topic: function() {
+                var promise = new EventEmitter();
+                var config_json = require.resolve('./files/config.json');
+                var config = new ConfigFile( {source: config_json, parser: function(data) {
+		    var value = JSON.parse(data);
+		    value.extra = "extra";
+		    delete value.test1;
+		    return value;
+		} } );
+		config.on('state', function(state_change) {
+		    if( state_change.state == 'invalid' ) {
+			promise.emit('failure', config);
+		    } else if ( state_change.state == 'ready' ) {
+			promise.emit('success', config);
+		    }
+		});
+                return promise;
+            },
+            'loads properly': function(config) {
+                assert.instanceOf( config, Configuration);
+                assert.instanceOf( config, ConfigFile );
+                assert.deepEqual( config.toObject(), { "extra":"extra", "test2": "b", "test3": {"c": "d", "e": ["f","g","h"] } , "test4": "i" } );
+            }        
+        },
+
     }
 }).export(module);
